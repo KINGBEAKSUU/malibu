43cefefed6e571be6352ca75fef7f517ba27881c
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-03-30 14:04:56 -0500

    EUV Value Table

--- selfdrive/car/gm/values.py
+++ selfdrive/car/gm/values.py
@@ -41,14 +41,14 @@ class CarControllerParams:
     self.ZERO_GAS = 6144  # Coasting
     self.MAX_BRAKE = 400  # ~ -4.0 m/s^2 with regen
 
-    if CP.carFingerprint in CAMERA_ACC_CAR and CP.carFingerprint not in CC_ONLY_CAR:
+    if CP.carFingerprint in CAMERA_ACC_CAR and CP.carFingerprint not in CC_ONLY_CAR and CP.carFingerprint != CAR.CHEVROLET_BOLT_EUV:
       self.MAX_GAS = 7496
       self.MAX_GAS_PLUS = 8848
       self.MAX_ACC_REGEN = 7110  # Increased for stronger regen braking
       self.INACTIVE_REGEN = 5650
       # Camera ACC vehicles have no regen while enabled.
       # Camera transitions to MAX_ACC_REGEN from ZERO_GAS and uses friction brakes instantly
-      self.max_regen_acceleration = -1.5
+      self.max_regen_acceleration = -3.0
 
     elif CP.carFingerprint in SDGM_CAR:
       self.MAX_GAS = 7496
@@ -77,8 +77,8 @@ class CarControllerParams:
   # determined by letting Volt regen to a stop in L gear from 89mph,
   # and by letting off gas and allowing car to creep, for determining
   # the positive threshold values at very low speed
-  EV_GAS_BRAKE_THRESHOLD_BP = [1.29, 1.52, 1.55, 1.6, 1.7, 1.8, 2.0, 2.2, 2.5, 5.52, 9.6, 20.5, 23.5, 35.0] # [m/s]
-  EV_GAS_BRAKE_THRESHOLD_V = [0.0, -0.14, -0.16, -0.18, -0.215, -0.255, -0.32, -0.41, -0.5, -0.72, -0.895, -1.125, -1.145, -1.16] # [m/s^s]
+  EV_GAS_BRAKE_THRESHOLD_BP = [1.12, 3.35, 5.59, 7.82, 10.06, 12.29, 14.53, 16.76, 19.0, 21.23, 23.47, 25.7, 27.94, 30.18, 32.41, 34.65],
+  EV_GAS_BRAKE_THRESHOLD_V = [-0.49, -0.59, -0.64, -0.66, -0.68, -0.78, -0.93, -1.0, -1.09, -1.1, -1.23, -1.28, -1.35, -1.4, -1.41, -1.46],
 
   def update_ev_gas_brake_threshold(self, v_ego):
     gas_brake_threshold = interp(v_ego, self.EV_GAS_BRAKE_THRESHOLD_BP, self.EV_GAS_BRAKE_THRESHOLD_V)
