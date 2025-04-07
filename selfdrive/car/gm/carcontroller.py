fc630c990f446cb7a5223273447436b7613b5be0
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-04-06 23:23:44 -0500

    No pedal scaling

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -81,10 +81,7 @@ class CarController(CarControllerBase):
     gain = interp(car_velocity, speed_mps, regen_gain_ratio)
 
     pedaloffset = interp(car_velocity, [0., 3, 6, 30], [0.10, 0.175, 0.240, 0.240])
-    if press_regen_paddle:
-      scaled_accel = accel * gain
-    else:
-      scaled_accel = accel
+    scaled_accel = accel
     pedal_gas = clip(pedaloffset + scaled_accel * 0.6, 0.0, 1.0)
 
     return pedal_gas, press_regen_paddle
