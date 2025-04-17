95bc7aed759a3b1d4b70e8faddc17ec795ed75ac
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-04-16 23:54:51 -0500

    No pedal_gas = 0

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -96,8 +96,6 @@ class CarController(CarControllerBase):
       scaled_accel = accel
 
     pedal_gas = clip(pedaloffset + scaled_accel * 0.6, 0.0, 1.0)
-    if scaled_accel < accel_cutoff:
-      pedal_gas = 0.0
 
     return pedal_gas, press_regen_paddle
 
